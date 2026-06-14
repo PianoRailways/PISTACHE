6279e602bb8512802464635d03f1dd4fb2498eb6
PianoRailways <piano@stellwerksim.ch>
2026-06-15 00:21:37 +0200

    Footer flicken damito nicht überdeckt??

--- blm/index.php
+++ blm/index.php
@@ -92,6 +92,7 @@ $has_entries = !empty($rows);
             left: 0;
             width: 100%;
             background: #222222;
+            margin-top: 40px;
             border-top: 2px solid #eb0000;
             padding: 15px 0;
             box-shadow: 0 -4px 10px rgba(0, 0, 0, 0.3);
